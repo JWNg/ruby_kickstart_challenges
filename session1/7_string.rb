5818ca7debc0a9dc08253d930c0e5c18bf4143e5
JWNg <jeff28_88@hotmail.com>
2016-03-26 20:47:04 -0400

    Update 7_string.rb

--- session1/7_string.rb
+++ session1/7_string.rb
@@ -1,3 +1,9 @@
+# given a string, return the character after every letter "r"
+#
+# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+
 def pirates_say_arrrrrrrrr(string)
   holder = String.new
   i = 0
@@ -7,3 +13,7 @@ def pirates_say_arrrrrrrrr(string)
   end
   holder
 end
+
+# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
