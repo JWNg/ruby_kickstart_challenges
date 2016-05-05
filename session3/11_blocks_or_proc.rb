b11ef1c5ed5980530a2e40c3252e055fdf4c7caa
JWNg <jeff28_88@hotmail.com>
2016-05-05 06:01:39 -0400

    Update 11_blocks_or_proc.rb

--- session3/11_blocks_or_proc.rb
+++ session3/11_blocks_or_proc.rb
@@ -30,5 +30,15 @@
 # end
 
 
-def array_init
+
+def array_init(n = 5, &block)
+  block ||= Proc.new{|i| (i*100).to_s}
+  Array.new(n, &block)  
 end
+
+p array_init(2) { |i| (i).to_s }
+p array_init { |i| i.to_s } 
+p array_init 2   
+p array_init 
+p array_init { 'hi' }
+
