aca82acf94f38d45fa122d4c0e1bea1d52a86048
JWNg <jeff28_88@hotmail.com>
2016-05-19 06:08:01 -0400

    Create 8_exceptions.rb

---
+++ session4/8_exceptions.rb
@@ -0,0 +1,25 @@
+# Write a method that receives a number 1 through 5.
+# Depending on the value of the number, it will raise
+# some particular type of exception.
+#
+# HINT:
+#   All exceptions and errors are descendants of Exception.
+#   (Exception is listed as an ancestor)
+#
+# Examples:
+#   exception_raiser 1   # =>   #<RuntimeError: No 1s allowed!>
+#   exception_raiser 2   # =>   #<ArgumentError: No 2s allowed!>
+#   exception_raiser 3   # =>   #<Exception: No 3s allowed!>
+#   exception_raiser 4   # =>   #<SyntaxError: No 4s allowed!>
+#   exception_raiser 5   # =>   #<RubyKickstartException: No 5s allowed!>
+
+def exception_raiser(num)
+  raise "No 1s allowed!" if num == 1
+  raise(ArgumentError, "No 2s allowed!") if num == 2
+  raise(Exception, "No 3s allowed!") if num == 3
+  raise(SyntaxError, "No 4s allowed!") if num == 4
+  raise(RubyKickstartException, "No 5s allowed!") if num == 5
+end
+
+class RubyKickstartException < Exception
+end
