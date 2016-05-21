4b7bedd26e1c9e7485de66ce2d15e58b4c3c1c4b
JWNg <jeff28_88@hotmail.com>
2016-05-21 05:56:30 -0400

    Update 1_singleton_class_string_exception.rb

--- session5/1_singleton_class_string_exception.rb
+++ session5/1_singleton_class_string_exception.rb
@@ -1,30 +1,28 @@
-# This is the same as 4:6 but now we want to make this more comprehensive
-#
-# There are a few nuances, though.
-# The String you return must be retained during the object's entire life
-# The method must be able to be called multiple times
-# The String you return should know how to add new CSS classes: each class is separated by a space
-# If someone tries to use + or []= or * on the String, you should raise a RuntimeError 
-# with a message of "use << method instead"
-# If they try to add the same String more than once, you should simply do nothing
-#
-# 0------------------------------
-#
-# EXAMPLE:
-#   controller = ApplicationController.new
-#   controller.body_class                 # => ""
-#   controller.body_class << 'admin'
-#   controller.body_class                 # => "admin"
-#   controller.body_class << 'category'
-#   controller.body_class                 # => "admin category"
-#   controller.body_class << 'page' << 'order'
-#   controller.body_class                 # => "admin category page order"
-#   controller.body_class << 'category'
-#   controller.body_class                 # => "admin category page order"
-#   controller.body_class + 'landing'     # => #<RuntimeError: use << method instead>
-#
-
 class ApplicationController  
+    
   def body_class
+    @body = String.new if @body.nil?
+    return @body if @body
+    
+    def @body.<<(input)
+      return self if self[/\b#{input}\b/]
+      concat " " unless self.length.zero?       
+      concat input   
+      end  
+    end
+    
+    def @body.+(input)    runtimeerror end
+    def @body.*(input)    runtimeerror end      
+    def @body.[]=(input)  runtimeerror end     
+    
+    private
+    def runtimeerror
+      raise(RuntimeError, "use << method instead")
+    end  
+    
+    def duplicate_check(input)
+      @body[input] ? true : false
+    end
+               
   end
 end
