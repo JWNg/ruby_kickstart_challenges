19bb5774b07e2c0a4fc35dae059e6daabab94671
JWNg <jeff28_88@hotmail.com>
2016-05-22 10:33:10 -0400

    Update 3_modules.rb

--- session5/3_modules.rb
+++ session5/3_modules.rb
@@ -23,6 +23,32 @@
 # ff.min                        # => 1
 # ff.select { |num| num.odd? }  # => [1, 5, 3]
 
+
+
 class List
+  include Enumerable
+  def inspect
+    output = "#<List: "
+    @values.length == 0 ? list_values = nil : @values.each{|v| output << v.to_s + ", "}
+    output.chop!.chop! if output[-2..-1] == ", "
+    output << ">"
+  end
   
+  def initialize(*values)
+    @values = values
+    self
+  end
+  
+  def <<(value)
+    @values << value
+    self
+  end
+    
+  def each(&block)
+    @values.each{|v| block.call(v)}
+  end   
 end
+
+list = List.new
+list << 1 << 5 << 3 << 4 << 2
+list.inspect
